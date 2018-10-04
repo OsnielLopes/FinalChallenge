75b18af4e3f2a1b4be96a33b26f5a0bd18f6c06b
Ana Carolina dos Santos Silva <ana.carolinasantos@icloud.com>
2018-10-04 13:54:15 -0300

    Add Viper classes

---
+++ FinalChallenge/FinalChallenge/Source/Daily Questions/DailyQuestionProtocols.swift
@@ -0,0 +1,37 @@
+//
+//  DailyQuestionProtocols.swift
+//  FinalChallenge
+//
+//  Created Ana Carolina Silva on 04/10/18.
+//  Copyright © 2018. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Router
+
+protocol DailyQuestionRouterProtocol: class {
+
+}
+
+// MARK: - Interactor
+
+protocol DailyQuestionInteractorInputProtocol {
+
+}
+
+// MARK: - Presenter
+
+protocol DailyQuestionPresenterInputProtocol: class {
+
+}
+
+protocol DailyQuestionInteractorOutputProtocol: class {
+
+}
+
+// MARK: - View
+
+protocol DailyQuestionPresenterOutputProtocol: class {
+
+}
---
+++ FinalChallenge/FinalChallenge/Source/Daily Questions/DailyQuestionView.swift
@@ -0,0 +1,27 @@
+//
+//  DailyQuestionView.swift
+//  FinalChallenge
+//
+//  Created Ana Carolina Silva on 04/10/18.
+//  Copyright © 2018. All rights reserved.
+//
+
+import UIKit
+
+class DailyQuestionView: UIViewController, DailyQuestionPresenterOutputProtocol {
+
+	// MARK: - Viper Module Properties
+
+	var presenter: DailyQuestionPresenterInputProtocol!
+
+	// MARK: - Override methods
+
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - DailyQuestionPresenterOutputProtocol
+
+	// MARK: - Private Methods
+
+}
