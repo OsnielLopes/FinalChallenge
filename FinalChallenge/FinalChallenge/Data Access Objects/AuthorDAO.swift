944d517264508df56a817ed746d2d30b3ffc1acf
guilhermePaciulli <guilherme.paciulli@icloud.com>
2018-10-09 15:43:41 -0300

    author dao and tests

--- FinalChallenge/FinalChallenge/Data Access Objects/AuthorDAO.swift
+++ FinalChallenge/FinalChallenge/Data Access Objects/AuthorDAO.swift
@@ -14,12 +14,42 @@ class AuthorDAO {
     
     private init() { }
     
-    func create(authorName: String, completion: @escaping (Author, DataAccessError) -> (Void)) {
+    func fetchAll(completion: @escaping ([Author]?, DataAccessError?) -> (Void)) {
+        
+        if let allAuthors = CoreDataManager.shared.fetch(Author.self) {
+            completion(allAuthors, nil)
+        } else {
+            completion(nil, DataAccessError(message: "Error when fetching all authors"))
+        }
+    }
+    
+    func create(authorName: String, completion: @escaping (Author?, DataAccessError?) -> (Void)) {
         
         if let newAuthor = CoreDataManager.shared.create(type: Author.self) {
+            newAuthor.name = authorName
             
+            completion(newAuthor, nil)
+        } else {
+            completion(nil, DataAccessError(message: "There was a problem creating an author"))
         }
         
+    }
+    
+    func update(author: Author, name: String? = nil, completion: @escaping (Author?, DataAccessError?) -> (Void)) {
+        
+        if let updatedName = name {
+            author.name = updatedName
+        }
+        
+        CoreDataManager.shared.saveContext()
+        
+        completion(author, nil)
+        
+    }
+    
+    func delete(author: Author, completion: @escaping (DataAccessError?) -> (Void)) {
+        CoreDataManager.shared.delete(author)
+        completion(nil)
     }
     
 }
--- FinalChallenge/FinalChallengeTests/Data Access Objects Tests/AuthorDAOTests.swift
+++ FinalChallenge/FinalChallengeTests/Data Access Objects Tests/AuthorDAOTests.swift
@@ -9,9 +9,11 @@
 import XCTest
 import CoreData
 
+@testable import FinalChallenge
+
 class AuthorDAOTests: XCTestCase {
 
-    var createdAuthor: [Author]!
+    var createdAuthors: [Author]!
     
     lazy var managedObjectModel: NSManagedObjectModel = {
         let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle(for: type(of: self))] )!
@@ -37,31 +39,64 @@ class AuthorDAOTests: XCTestCase {
     }()
     
     func generateData() {
+        self.createdAuthors = []
+        for i in 0...5 {
+            let c = NSEntityDescription.insertNewObject(forEntityName: "Author", into: self.mockPersistantContainer.viewContext) as! Author
+            c.name = "Author\(i)"
+            self.createdAuthors.append(c)
+        }
+        try! self.mockPersistantContainer.viewContext.save()
     }
     
     func flushData() {
+        self.createdAuthors = []
+        let request: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Author")
+        let all = try! self.mockPersistantContainer.viewContext.fetch(request) as! [Author]
+        for i in all {
+            self.mockPersistantContainer.viewContext.delete(i)
+        }
+        
+        try! self.mockPersistantContainer.viewContext.save()
     }
     
     override func setUp() {
         super.setUp()
         self.generateData()
+        CoreDataManager.shared.persistentContainer = self.mockPersistantContainer
     }
 
     override func tearDown() {
         self.flushData()
-        super.setUp()
+        super.tearDown()
     }
 
     func testIfCreatesAnAuthor() {
+        AuthorDAO.shared.create(authorName: "User", completion: { (author, err) in
+            XCTAssertNil(err)
+            XCTAssertNotNil(author)
+        })
     }
     
     func testIfFetchesAuthors() {
+        AuthorDAO.shared.fetchAll(completion: { (authors, err) in
+            XCTAssertNil(err)
+            XCTAssertNotNil(authors)
+            XCTAssert(authors!.count == 6)
+        })
     }
     
-    func testIfUpdatesAuthor() {
+    func testIfUpdatesAnAuthor() {
+        AuthorDAO.shared.update(author: self.createdAuthors[0], name: "Other user", completion: { (author, err) in
+            XCTAssertNil(err)
+            XCTAssertNotNil(author)
+            XCTAssertEqual(author!.name, "Other user")
+        })
     }
     
-    func testIfDeletesAuthor() {
+    func testIfDeletesAnAuthor() {
+        AuthorDAO.shared.delete(author: self.createdAuthors[0], completion: { err in
+            XCTAssertNil(err)
+        })
     }
 
 }
