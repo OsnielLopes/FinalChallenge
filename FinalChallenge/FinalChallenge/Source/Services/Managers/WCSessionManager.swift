b7a7450add238e5d440618df4a66da66a644ca3b
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2018-12-03 15:06:01 -0200

    session config

--- FinalChallenge/FinalChallenge/Source/Services/Managers/WCSessionManager.swift
+++ FinalChallenge/FinalChallenge/Source/Services/Managers/WCSessionManager.swift
@@ -12,28 +12,31 @@ class WSManager: NSObject, WCSessionDelegate {
     
     public static let shared = WSManager()
     public var recievedMessage: (([String : Any]) -> Void)?
-    private var validSession: WCSession? {
-        
+    private var validSession: WCSession? /*{
+
         // paired - the user has to have their device paired to the watch
         // watchAppInstalled - the user must have your watch app installed
-        
+
         // Note: if the device is paired, but your watch app is not installed
         // consider prompting the user to install it for a better experience
-        
+
         if let session = self.session, (session.isPaired && session.isWatchAppInstalled) {
             return session
         }
         return nil
-    }
-    private let session: WCSession? = WCSession.isSupported() ? WCSession.default : nil
+    }*/
+//    private let session: WCSession? = WCSession.isSupported() ? WCSession.default : nil
 
     override private init() {
         super.init()
     }
     
     func startSession() {
-        self.validSession?.delegate = self
-        self.validSession?.activate()
+        if WCSession.isSupported() {
+            self.validSession = WCSession.default
+            self.validSession?.delegate = self
+            self.validSession?.activate()
+        }
     }
     
     func sessionDidBecomeInactive(_ session: WCSession) {
@@ -51,7 +54,7 @@ class WSManager: NSObject, WCSessionDelegate {
     }
     
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        
+        print("olá")
     }
     
     func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
